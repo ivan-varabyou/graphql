b0e3f9059bbbfb26285890281e8e9ae8555e09df
Varabyou Ivan <onebelbiz@gmail.com>
2023-11-29 14:13:09 +0300

    faet: entity binding graphql

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,27 +1,60 @@
 const graphql = require("graphql");
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 const movies = [
   {
     id: 1,
     name: "The Shawshank Redemption",
     genre: "Drama",
+    directorId: 1,
   },
   {
     id: 2,
     name: "The Godfather",
     genre: "Crime",
+    directorId: 2,
   },
   {
     id: "3",
     name: "The Dark Knight",
     genre: "Action",
+    directorId: 3,
   },
   {
     id: "4",
     name: "The Dark Knight New",
     genre: "Action New",
+    directorId: 4,
+  },
+];
+
+const directors = [
+  {
+    id: 1,
+    name: "Steven Spielberg",
+    age: 74,
+  },
+  {
+    id: 2,
+    name: "Christopher Nolan",
+    age: 51,
+  },
+  {
+    id: 3,
+    name: "Quentin Tarantino",
+    age: 58,
+  },
+  {
+    id: 70,
+    name: "Alex Tarantino",
+    age: 70,
   },
 ];
 
@@ -31,6 +64,20 @@ const MoviesType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    directorId: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find((director) => director.id == parent.id);
+      },
+    },
+  }),
+});
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -43,6 +90,13 @@ const Query = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         return movies.find((movie) => movie.id == args.id);
+      },
+    },
+    directors: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return directors.find((director) => director.id == args.id);
       },
     },
   },
